bc30f4c09c24440e3a38b9cc6449525de33ff948
⚘ᴩᴀᴛʜɪʀᴀᴊ.Py™♨ヅ <leoyagami123123@gmail.com>
2026-01-04 18:38:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from logging.handlers import RotatingFileHandler
 
 # Bot Configuration
 LOG_FILE_NAME = "bot.log"
-PORT = '8090'
+PORT = '8080'
 OWNER_ID = 8372956796
 
 MSG_EFFECT = 5159385139981059251
