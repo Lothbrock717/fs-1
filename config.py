9b0c1081feb219f1389864573b81d2a235c59c1c
Lothbrock717 <91334582+Lothbrock717@users.noreply.github.com>
2026-03-25 08:25:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ DB_NAME = "yato"
 
 FSUBS = [[-1003597446654, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL = -1003841231246# just put channel id dont add ""
+DB_CHANNEL = -1003737655339# just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 #DB_CHANNELS = {
 #     "-1002419692267": {"name": "Primary DB", "is_primary": True, "is_active": True},
