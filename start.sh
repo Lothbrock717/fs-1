ddf37d36f8dc07f3de8c690cbceb98929154c34d
Lothbrock717 <91334582+Lothbrock717@users.noreply.github.com>
2026-04-12 21:23:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-git pull origin $(git rev-parse --abbrev-ref HEAD) 2>/dev/null || echo "Git pull skipped"
-pip install -r requirements.txt --no-cache-dir
-python3 main.py
+#!/bin/bash
+cd /app
+git pull origin Yato
+python main.py
