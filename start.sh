116c7a71777d07f4e8d68111faa49996083a356a
Lothbrock717 <91334582+Lothbrock717@users.noreply.github.com>
2026-04-12 21:44:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+cp /config.py /app/config.py
 cd /app
 git fetch origin Yato
 git reset --hard origin/Yato
 cp /config.py /app/config.py
 pip install -r requirements.txt -q
+cp /config.py /app/config.py
 python main.py
