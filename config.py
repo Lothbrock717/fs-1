3398da437e5b9d2007a731ec286bc2172f883308
Lothbrock717 <91334582+Lothbrock717@users.noreply.github.com>
2026-04-22 03:18:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,18 +13,18 @@ SHORT_API = ""
 SHORT_TUT = ""
 
 # Bot Configuration
-SESSION = "yato"
-TOKEN = "8658777630:AAF5KxCxoL_u2P0feCoAh8M9G2BNSW5_MIc"
+SESSION = ""
+TOKEN = ""
 API_ID = "26444821"
 API_HASH = "a58efd1d6483e3f0d5b2757d9f665c24"
 WORKERS = 10
 
 DB_URI = "mongodb+srv://abhay:store@store.nmvpb5w.mongodb.net/?retryWrites=true&w=majority&appName=STORE"
-DB_NAME = "yato"
+DB_NAME = ""
 
 FSUBS = [[-1003597446654, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL = -1003737655339# just put channel id dont add ""
+DB_CHANNEL =  # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 #DB_CHANNELS = {
 #     "-1002419692267": {"name": "Primary DB", "is_primary": True, "is_active": True},
