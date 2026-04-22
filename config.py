236485fd338196ca89bacd94587304c15fb4b1d7
Lothbrock717 <91334582+Lothbrock717@users.noreply.github.com>
2026-04-23 01:45:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ WORKERS = 10
 DB_URI = ""
 DB_NAME = ""
 
-FSUBS = [[-1003597446654, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
+FSUBS = [[-1003597446654, False, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
 DB_CHANNEL =  # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
